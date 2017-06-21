6feba8167a6f5cf5edcfe48398d09fa87e9e70c2
Eran Shabi <eranshabi@gmail.com>
2017-06-21 18:34:27 +0300

    add protractor iframe height test

--- test/e2e/app.e2e.js
+++ test/e2e/app.e2e.js
@@ -2,9 +2,11 @@ import 'babel-polyfill';
 
 describe('React application', () => {
   describe('open page', () => {
-    it('should display title', async () => {
-      await browser.get('/');
-      expect(await $('h2').getText()).toBe('Hello World!');
+    it('should update iframe height to initial height', async () => {
+      await browser.get('/iframeContainer');
+      //await browser.sleep(200);
+      const iframeSize = await $('[data-hook="iframe-container"]').getSize();
+      expect(iframeSize.height).toBe(100);
     });
   });
 });
